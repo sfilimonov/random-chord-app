e4d8faaed2a577ee61e70db49b90df0cfc44e773
Sergey Filimonov <sergey.f@me.com>
2012-11-06 17:06:33 +0400

    new
    new

---
+++ _/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- Title & Meta -->
+	<title>Random Chord</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" CONTENT="" />
+	<meta name="Keywords" content="" />
+	<!--<link rel="icon" type="image/png" href="http://madewith.ru/favicon.ico">-->
+	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<link rel="stylesheet" href="/css/font-awesome.css">
+	<link rel="stylesheet/less" type="text/css" href="/less/bootstrap.less">
+	<link rel="stylesheet" type="text/css" href="/css/custom.css">
+	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	
+	<!-- JS -->
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type="text/javascript" src="/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<script src="/js/less.js" type="text/javascript"></script>
+	<script type="text/javascript" src="/js/modernizr.js"></script>
+	
+	<!-- iOS -->
+	<link rel="apple-touch-icon" href="img/apple-touch-icon-iphone.png" /> 
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" /> 
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-iphone4.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-ipad3.png" />
+
+</head>
+<body>
---
+++ _/index.php
@@ -0,0 +1,70 @@
+<?php include("header.php"); ?>
+
+<body>
+<div class="container">
+	<div class="span10">
+		<div class="span2">
+			<h2>Major</h2>
+			<section class="major">
+				<ul>
+					<li>A</li>
+					<li>B</li>
+					<li>C</li>
+					<li>D</li>
+					<li>E</li>
+					<li>F</li>
+					<li>G</li>
+				</ul>
+			</section>
+		</div>
+		<div class="span2">
+			<section class="minor">
+				<h2>Minor</h2>
+				<ul>
+					<li>Am</li>
+					<li>Bm</li>
+					<li>Cm</li>
+					<li>Dm</li>
+					<li>Em</li>
+					<li>Fm</li>
+					<li>Gm</li>
+				</ul>
+			</section>
+		</div>
+		<div class="span2">
+			<section class="7th">
+			<h2>7th</h2>
+				<ul>
+					<li>A7</li>
+					<li>B7</li>
+					<li>C7</li>
+					<li>D7</li>
+					<li>E7</li>
+					<li>F7</li>
+					<li>G7</li>
+				</ul>
+			</section>
+		</div>
+		<div class="span2">
+			<section class="5th">
+				<h2>5th</h2>
+				<ul>
+					<li>A5</li>
+					<li>B5</li>
+					<li>C5</li>
+					<li>D5</li>
+					<li>E5</li>
+					<li>F5</li>
+					<li>G5</li>
+				</ul>
+			</section>
+		</div>
+	</div>
+	<div class="span 12">
+		<button class="btn btn-large btn-primary" type="button">Play</button>
+	</div>
+	
+</div>
+</body>
+
+<?php include("footer.php"); ?>
